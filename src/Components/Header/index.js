3363f319b385d7fbb63bd49586bb85c3fc990ad9
joseliraa <josmliraa@gmail.com>
2022-07-06 21:13:17 -0600

    Header component

---
+++ src/Components/Header/index.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import { View} from 'react-native';
+import styles from './styles';
+import InputsRegister from '../InputsRegister';
+import TitleLogin from '../TitleLogin';
+
+const Header = () => {
+  
+    return (
+        <View style={styles.container}>
+        
+       </View>
+    );
+  };
+
+
+
+export default Header;
---
+++ src/Components/Header/styles.js
@@ -0,0 +1,8 @@
+import { StyleSheet } from "react-native";
+import {typePoke, backgroundType, texts} from "../../../assets/Colors";
+
+const styles = StyleSheet.create({
+ 
+});
+
+export default styles;
